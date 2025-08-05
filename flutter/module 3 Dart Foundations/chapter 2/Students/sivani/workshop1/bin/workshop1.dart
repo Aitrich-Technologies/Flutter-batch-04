0e8bc11dd8288e5a43442b31c3edef4217e09cea
Sivani <sivanins320@gmail.com>
2025-08-05 12:18:24 +0530

    added

--- flutter/module 3 Dart Foundations/chapter 2/Students/sivani/workshop1/bin/workshop1.dart
+++ flutter/module 3 Dart Foundations/chapter 2/Students/sivani/workshop1/bin/workshop1.dart
@@ -1,20 +1,17 @@
-void main()
-{
+void main() {
   var obj = cs();
   print(obj.b);
   print(obj.a);
 }
 
-class cs                                                         //created a class
+class cs //created a class
 {
-  var b ='This is variable b from the constructor';
-  var a = 'This is variable a and is a field';                  //created a field
+  var b = 'This is variable b from the constructor';
+  var a = 'This is variable a and is a field'; //created a field
 
-cs()                                                           // created constructor inside the class
-{
-
-  
+  cs() // created constructor inside the class
+  {
+    var a = 'This is variable a and is a field';
+    print(a);
+  }
 }
-
-}
-
