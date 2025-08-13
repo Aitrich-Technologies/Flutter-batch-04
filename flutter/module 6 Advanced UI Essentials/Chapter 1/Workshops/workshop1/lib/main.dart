fe22f2ea261e0e5500f79a4785f5cba8bf3a297d
Jubi-Aitrich <jubiaitrich@gmail.com>
2025-08-13 09:33:31 +0530

    Added

--- flutter/module 6 Advanced UI Essentials/Chapter 1/Workshops/workshop1/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:workshop2/workshop2.dart';
-
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Favorites2()
-  ));
-}
--- flutter/module 6 Advanced UI Essentials/Chapter 1/Workshops/workshop2/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:workshop3/workshop3.dart';
-
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Buyerviewpages1()
-  ));
-}
